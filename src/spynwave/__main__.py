aaf26ad786477a68df2a3c9e1664124f9818b728
Casper Schippers <Casperschippers@hotmail.com>
2022-11-07 09:57:00 +0100

    update `exec_` to `exec`

--- src/spynwave/__main__.py
+++ src/spynwave/__main__.py
@@ -25,7 +25,7 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
     window = Window()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
