4ae912d219c8928d0dcc379413172803ab6038ad
Casper Schippers <casperschippers@hotmail.com>
2022-11-17 15:38:39 +0100

    Extend description

--- src/spynwave/drivers/magnet_cryostat.py
+++ src/spynwave/drivers/magnet_cryostat.py
@@ -6,6 +6,8 @@ import logging
 
 from spynwave.drivers.magnet_base import MagnetBase
 
+# TODO: include the temperature controller, there is a python library from lakeshore for this
+
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
@@ -14,7 +16,7 @@ class MagnetCryostat(MagnetBase):
     """ This class represents the magnet that is used on the crystat/blackhole spinwave setup.
 
     It uses a LakeShore 475 Gaussmeter and a LakeShore 643 Power Supply, which is controlled by the
-    Gaussmeter
+    Gaussmeter. The setup also contains a LakeShore 336 Temperature Controller.
 
     """
     pass
