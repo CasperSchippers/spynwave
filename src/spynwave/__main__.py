a56b60f900ddf42917985e7c67f281da329020de
physfna4 <physfna4@tue.nl>
2022-11-23 10:37:35 +0100

    Add console logger

--- src/spynwave/__main__.py
+++ src/spynwave/__main__.py
@@ -6,18 +6,30 @@ import sys
 import logging
 
 from pymeasure.display.Qt import QtWidgets
+from pymeasure.log import setup_logging
 
 from spynwave.interface import Window
 
 
 # Initialize logger & log to file
+
+
 log = logging.getLogger("")
+
 file_handler = logging.FileHandler("SpynWave.log", "a")
 file_handler.setFormatter(logging.Formatter(
     fmt="%(asctime)s : %(message)s (%(levelname)s)",
     datefmt="%m/%d/%Y %I:%M:%S %p"
 ))
 log.addHandler(file_handler)
+
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setFormatter(logging.Formatter(
+    fmt="%(asctime)s : %(message)s (%(levelname)s)",
+    datefmt="%m/%d/%Y %I:%M:%S %p"
+))
+console_handler.setLevel(logging.DEBUG)
+log.addHandler(console_handler)
 
 
 def main():
