c9929ff04d3ad1a351c2e00b9aee59707e5f942f
Casper Schippers <casperschippers@hotmail.com>
2022-11-23 14:00:14 +0100

    Get MagnetAutoselect to work

--- src/spynwave/drivers/magnet_autoselect.py
+++ src/spynwave/drivers/magnet_autoselect.py
@@ -8,20 +8,23 @@ from spynwave.drivers.magnet_base import MagnetBase
 from spynwave.drivers import MagnetInPlane, MagnetCryostat, MagnetOutOfPlane
 
 
-class Magnet(MagnetBase):
-    """ A constructor class that detects which magnet to use"""
+class MagnetConstructor(type):
+    def __call__(cls, *args, **kwargs):
+        return cls.get_magnet_class()(*args, **kwargs)
+
+
+class Magnet(type(MagnetBase), metaclass=MagnetConstructor):
+    """ A constructor class that detects which magnet to use. This will never be
+    actually instanced, since calling the class [Magnet()] will call the __call__ method
+    of the MagnetConstructor metaclass and return an instance of the appropriate magnet
+    class.
+    """
+
     magnet_classes = {
         "in-plane magnet": MagnetInPlane,
         "out-of-plane magnet": MagnetOutOfPlane,
         "cryo magnet": MagnetCryostat,
     }
-
-    def __new__(cls, *args, **kwargs):
-        # Detect which magnet is used
-
-        MagnetClass = cls.get_magnet_class()
-
-        return MagnetClass.__new__(MagnetClass, *args, **kwargs)
 
     @classmethod
     def get_magnet_class(cls):
